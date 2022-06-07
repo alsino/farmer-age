09a902719dc3049949565bcee187b4485f52c45d
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2022-06-07 10:54:18 +0200

    Update config-features.js

--- src/lib/stores/config-features.js
+++ src/lib/stores/config-features.js
@@ -3,7 +3,7 @@ export const config = {
   "vercelURL": "farmer-age.vercel.app", // The url provided when deploying the map on Vercel
   "datasetType": "values", // "values" or "binary"; Does the data set contain numerical values or binary (0/1) values for countries?
   "datasetUnit": "percent", // "fullNumbers" or "percent"; Is the data in percent (0.25 of GDP) or full numbers (25 people)?
-  "colourScheme": "blue", // one of the following: "blue", "purple-blue", "green-blue", "orange-red"
+  "colourScheme": "green-blue", // one of the following: "blue", "purple-blue", "green-blue", "orange-red"
   "headlineAvailable": true, // true or false; Should the map have a headline? 
   "subheadlineAvailable": true, // true or false; Should the map have a subheadline? 
   "tooltipAvailable": true, // true or false; Should the map show a tooltip when hovering over a country? 
